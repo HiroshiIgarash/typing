f9f383b65d4cc418e483e0df0c39536338d9c52e
HiroshiIgarashi <tubukiti2008@gmail.com>
2022-05-24 23:29:24 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,245 @@
+//お題データ
+data1=["Tsukino Mito","Yuki Chihiro","Elu","Higuchi Kaede","Shizuka Rin","Shibuya Hajime","Suzuya Aki","Moira","Suzuka Utako","Ushimi Ichigo","Ienaga Mugi","Yuhi Riri","Mononobe Alice","Fumino Tamaki","Fushimi Gaku","Gilzaren III","Kenmochi Toya","Morinaka Kazaki","Kanae","Akabane Youko","Sasaki Saku","Yamiyono Moruru","Honma Himawari","Makaino Ririmu","Kuzuha","Setsuna","Shiina Yuika","Dola","Umiyashanokami","Nakao Azuma","Izumo Kasumi","Todoroki Kyoko","Sister Claire","Hanabatake Chaika","Yashiro Kizuku","Azuchi Momo","Suzuki Masaru","Ryushen","Uzuki Kou","Hassaku Yuzu","Kanda Shoichi","Naruto Kogane","Asuka Hina","Harusaki Air","Amemori Sayo","Takamiya Rion","Maimoto Keisuke","Rindou Mikoto","Debidebi Debiru","Sakura Ritsuki","Machita Chima","Tsukimi Shizuku","Joe Rikiichi","Achikita Chinami","Naruse Naru","Belmond Banderas","Yaguruma Rine","Yumeoi Kakeru","Kuroi Shiba","Warabeda Meiji","Kudo Chitose","Gundo Mirei","Yuzuki Roa","Onomachi Haruka","Kataribe Tsumugu","Seto Miyako","Otogibara Era","Inui Toko","Ange Katrina","Lize Helesta","Saegusa Akina","Aizono Manami","Suzuhara Lulu","Yukishiro Mahiro","Ex Albio","Levi Elipha","Hayama Marin","Nui Sociere","Hakase Fuyuki","Kagami Hayato","Yorumi Rena","Mayuzumi Kai","Ars Almal","Aiba Uiha","Amamiya Kokoro","Eli Conifer","Ratna Petit","Hayase Sou","Sukoya Kana","Shellin Burgundy","Fumi","Hoshikawa Sara","Yamagami Karuta","Emma August","Luis Cammy","Matsukai Mao","Fuwa Minato","Shirayuki Tomoe","Gwelu Os Gar","Mashiro","Naraka","Kurusu Natsume","Furen E Lustario","Melissa Kinrenka","Ibrahim","Nagao Kei","Genzuki Tojiro","Kaida Haru","Sorahoshi Kirame","Asahina Akane","Suo Sango","Todo Kohaku","Kitakoji Hisui","Nishizono Chigusa","Axia Krone","Lauren Iroas","Leos Vincent","Oliver Evans","Lain Paterson","Amagase Muyu","Ponto Nei","Umise Yotsuha"];
+data=[{"kana":"月ノ美兎","eigo":"Tsukino Mito"},{"kana":"勇気ちひろ","eigo":"Yuki Chihiro"},{"kana":"える","eigo":"Elu"},{"kana":"樋口楓","eigo":"Higuchi Kaede"},{"kana":"静凛","eigo":"Shizuka Rin"},{"kana":"渋谷ハジメ","eigo":"Shibuya Hajime"},{"kana":"鈴谷アキ","eigo":"Suzuya Aki"},{"kana":"モイラ","eigo":"Moira"},{"kana":"鈴鹿詩子","eigo":"Suzuka Utako"},{"kana":"宇志海いちご","eigo":"Ushimi Ichigo"},{"kana":"家長むぎ","eigo":"Ienaga Mugi"},{"kana":"夕陽リリ","eigo":"Yuhi Riri"},{"kana":"物述有栖","eigo":"Mononobe Alice"},{"kana":"文野環","eigo":"Fumino Tamaki"},{"kana":"伏見ガク","eigo":"Fushimi Gaku"},{"kana":"ギルザレンIII世","eigo":"Gilzaren III"},{"kana":"剣持刀也","eigo":"Kenmochi Toya"},{"kana":"森中花咲","eigo":"Morinaka Kazaki"},{"kana":"叶","eigo":"Kanae"},{"kana":"赤羽葉子","eigo":"Akabane Youko"},{"kana":"笹木咲","eigo":"Sasaki Saku"},{"kana":"闇夜乃モルル","eigo":"Yamiyono Moruru"},{"kana":"本間ひまわり","eigo":"Honma Himawari"},{"kana":"魔界ノりりむ","eigo":"Makaino Ririmu"},{"kana":"葛葉","eigo":"Kuzuha"},{"kana":"雪汝","eigo":"Setsuna"},{"kana":"椎名唯華","eigo":"Shiina Yuika"},{"kana":"ドーラ","eigo":"Dola"},{"kana":"海夜叉神","eigo":"Umiyashanokami"},{"kana":"名伽尾アズマ","eigo":"Nakao Azuma"},{"kana":"出雲霞","eigo":"Izumo Kasumi"},{"kana":"轟京子","eigo":"Todoroki Kyoko"},{"kana":"シスター・クレア","eigo":"Sister Claire"},{"kana":"花畑チャイカ","eigo":"Hanabatake Chaika"},{"kana":"社築","eigo":"Yashiro Kizuku"},{"kana":"安土桃","eigo":"Azuchi Momo"},{"kana":"鈴木勝","eigo":"Suzuki Masaru"},{"kana":"緑仙","eigo":"Ryushen"},{"kana":"卯月コウ","eigo":"Uzuki Kou"},{"kana":"八朔ゆず","eigo":"Hassaku Yuzu"},{"kana":"神田笑一","eigo":"Kanda Shoichi"},{"kana":"鳴門こがね","eigo":"Naruto Kogane"},{"kana":"飛鳥ひな","eigo":"Asuka Hina"},{"kana":"春崎エアル","eigo":"Harusaki Air"},{"kana":"雨森小夜","eigo":"Amemori Sayo"},{"kana":"鷹宮リオン","eigo":"Takamiya Rion"},{"kana":"舞元啓介","eigo":"Maimoto Keisuke"},{"kana":"竜胆尊","eigo":"Rindou Mikoto"},{"kana":"でびでび・でびる","eigo":"Debidebi Debiru"},{"kana":"桜凛月","eigo":"Sakura Ritsuki"},{"kana":"町田ちま","eigo":"Machita Chima"},{"kana":"月見しずく","eigo":"Tsukimi Shizuku"},{"kana":"ジョー・力一","eigo":"Joe Rikiichi"},{"kana":"遠北千南","eigo":"Achikita Chinami"},{"kana":"成瀬鳴","eigo":"Naruse Naru"},{"kana":"ベルモンド・バンデラス","eigo":"Belmond Banderas"},{"kana":"矢車りね","eigo":"Yaguruma Rine"},{"kana":"夢追翔","eigo":"Yumeoi Kakeru"},{"kana":"黒井しば","eigo":"Kuroi Shiba"},{"kana":"童田明治","eigo":"Warabeda Meiji"},{"kana":"久遠千歳","eigo":"Kudo Chitose"},{"kana":"郡道美玲","eigo":"Gundo Mirei"},{"kana":"夢月ロア","eigo":"Yuzuki Roa"},{"kana":"小野町春香","eigo":"Onomachi Haruka"},{"kana":"語部紡","eigo":"Kataribe Tsumugu"},{"kana":"瀬戸美夜子","eigo":"Seto Miyako"},{"kana":"御伽原江良","eigo":"Otogibara Era"},{"kana":"戌亥とこ","eigo":"Inui Toko"},{"kana":"アンジュ・カトリーナ","eigo":"Ange Katrina"},{"kana":"リゼ・ヘルエスタ","eigo":"Lize Helesta"},{"kana":"三枝明那","eigo":"Saegusa Akina"},{"kana":"愛園愛美","eigo":"Aizono Manami"},{"kana":"鈴原るる","eigo":"Suzuhara Lulu"},{"kana":"雪城眞尋","eigo":"Yukishiro Mahiro"},{"kana":"エクス・アルビオ","eigo":"Ex Albio"},{"kana":"レヴィ・エリファ","eigo":"Levi Elipha"},{"kana":"葉山舞鈴","eigo":"Hayama Marin"},{"kana":"ニュイ・ソシエール","eigo":"Nui Sociere"},{"kana":"葉加瀬冬雪","eigo":"Hakase Fuyuki"},{"kana":"加賀美ハヤト","eigo":"Kagami Hayato"},{"kana":"夜見れな","eigo":"Yorumi Rena"},{"kana":"黛灰","eigo":"Mayuzumi Kai"},{"kana":"アルス・アルマル","eigo":"Ars Almal"},{"kana":"相羽ういは","eigo":"Aiba Uiha"},{"kana":"天宮こころ","eigo":"Amamiya Kokoro"},{"kana":"エリー・コニファー","eigo":"Eli Conifer"},{"kana":"ラトナ・プティ","eigo":"Ratna Petit"},{"kana":"早瀬走","eigo":"Hayase Sou"},{"kana":"健屋花那","eigo":"Sukoya Kana"},{"kana":"シェリン・バーガンディ","eigo":"Shellin Burgundy"},{"kana":"フミ","eigo":"Fumi"},{"kana":"星川サラ","eigo":"Hoshikawa Sara"},{"kana":"山神カルタ","eigo":"Yamagami Karuta"},{"kana":"えま★おうがすと","eigo":"Emma August"},{"kana":"ルイス・キャミー","eigo":"Luis Cammy"},{"kana":"魔使マオ","eigo":"Matsukai Mao"},{"kana":"不破湊","eigo":"Fuwa Minato"},{"kana":"白雪巴","eigo":"Shirayuki Tomoe"},{"kana":"グウェル・オス・ガール","eigo":"Gwelu Os Gar"},{"kana":"ましろ","eigo":"Mashiro"},{"kana":"奈羅花","eigo":"Naraka"},{"kana":"来栖夏芽","eigo":"Kurusu Natsume"},{"kana":"フレン・E・ルスタリオ","eigo":"Furen E Lustario"},{"kana":"メリッサ・キンレンカ","eigo":"Melissa Kinrenka"},{"kana":"イブラヒム","eigo":"Ibrahim"},{"kana":"長尾景","eigo":"Nagao Kei"},{"kana":"弦月藤士郎","eigo":"Genzuki Tojiro"},{"kana":"甲斐田晴","eigo":"Kaida Haru"},{"kana":"空星きらめ","eigo":"Sorahoshi Kirame"},{"kana":"朝日南アカネ","eigo":"Asahina Akane"},{"kana":"周央サンゴ","eigo":"Suo Sango"},{"kana":"東堂コハク","eigo":"Todo Kohaku"},{"kana":"北小路ヒスイ","eigo":"Kitakoji Hisui"},{"kana":"西園チグサ","eigo":"Nishizono Chigusa"},{"kana":"アクシア・クローネ","eigo":"Axia Krone"},{"kana":"ローレン・イロアス","eigo":"Lauren Iroas"},{"kana":"レオス・ヴィンセント","eigo":"Leos Vincent"},{"kana":"オリバー・エバンス","eigo":"Oliver Evans"},{"kana":"レイン・パターソン","eigo":"Lain Paterson"},{"kana":"天ヶ瀬むゆ","eigo":"Amagase Muyu"},{"kana":"先斗寧","eigo":"Ponto Nei"},{"kana":"海妹四葉","eigo":"Umise Yotsuha"}]
+
+//お題
+const TIMEBONUS_COMBO=30; //ボーナスを得るためのコンボ数
+const TIMEBONUS_TIME=15; //ボーナスタイム
+const START_TIME=20; //スタート時のタイム
+const GAMEOVER_MISS=50; //ゲームオーバーになるミス数
+let theme=data[0].eigo //打つべき文字
+let strNum=0; //現在の文字場所
+let audioCorrect=new Audio("correct.mp3") //タイピング音
+let audioMiss=new Audio("miss.mp3") //ミス音
+let audioBonus=new Audio("timeBonus.mp3")
+let combo=0; //現在のコンボ
+let MaxCombo=0; //Maxコンボ
+let score=0; //スコア
+let type=0; //タイプ数
+let miss=0; //ミス数
+let time=START_TIME; //残り時間
+let gameover=true;
+let timer=null;
+
+
+$(".result").hide();
+
+//変数の初期化
+function init(){
+    combo=0;
+    MaxCombo=0;
+    score=0;
+    type=0;
+    miss=0;
+    time=START_TIME;
+
+    $("#Combo").html(combo);
+    $("#MaxCombo").html(MaxCombo);
+    $("#Score").html(score);
+    $("#Miss").html(miss);
+    $("#Type").html(type);
+    $("#Timer").html(time);
+    $(".bar").css("width",time*6+"px")
+    $(".scorebar").css("width",0)
+}
+
+//お題をspanで囲みセットする
+function createSpan(){
+    //文字ごとに分解
+    arrTheme=theme.split("")
+
+    //#themeを空に
+    $("#theme").empty();
+    
+    //spanで囲んであげる
+    arrTheme.forEach(spell => {
+        $("#theme").append(`<span>${spell}</span>`);
+    });
+}
+
+//文字を打った時
+$("#nyuryoku").on("input",()=>{
+    //打ち込んだ文字
+    val=$("#nyuryoku").val();
+
+    //menu画面の時
+    if(gameover){
+        //スペースキーでゲーム開始
+        if(val==" "){
+            gameover=false;
+            init();
+            $(".menu").hide();
+            $(".result").hide();
+            themeset();
+            timer=setInterval(()=>{
+                time--;
+                $("#Timer").html(time);
+                $(".bar").css("width",time*6+"px")
+                if(time<1){
+                    clearInterval(timer);
+                    gameover=true;
+                    $(".result .score").html(score);
+                    $(".result .type").html(type);
+                    $(".result .maxCombo").html(MaxCombo);
+                    $(".result .miss").html(miss);
+                    $(".result .missper").html((type==0?0:Math.floor(miss/type*10000)/100)+" %");
+                    $(".result .result-rank").html(rank(score));
+
+                    $(".result").show();
+                }
+            },1000)     
+        }
+
+    //ゲーム中
+    }else{
+
+    type++
+    $("#Type").html(type)
+
+    //あっている場合
+    if(theme[strNum]==val){
+        //combo+
+        combo++
+        MaxCombo=Math.max(combo,MaxCombo)
+        $("#Combo").html(combo)
+        $("#MaxCombo").html(MaxCombo)
+
+        //comboがTIMEBONUS_COMBOの倍数で時間延長
+        if(combo>0&&combo%TIMEBONUS_COMBO==0){
+            time+=TIMEBONUS_TIME;
+            showmessage(`+${TIMEBONUS_TIME}s`,"green","0","500px")
+            audioBonus.currentTime=0;
+            audioBonus.play();
+        }
+        
+        //score
+        score+=Math.floor((1.02**combo))*10
+        $("#Score").html(score)
+        $(".scorebar").css("width",score/1000000*85+"%")
+        
+        //赤字で獲得スコア表示
+        showmessage("+"+Math.floor((1.02**combo))*10,"red")
+        
+        //効果音
+        audioCorrect.currentTime=0;
+        audioCorrect.play();
+        
+        //correctクラスの追加
+        $(`#theme span:nth-child(${strNum+1})`).addClass("correct");
+        strNum++;
+        addUnder();
+        
+        if(strNum==theme.length){
+            themeset();
+        }
+    }else{
+        //効果音
+        audioMiss.currentTime=0;
+        audioMiss.play();
+        
+        //青字でミス
+        showmessage("Miss","blue");
+        
+        
+        miss++
+        $("#Miss").html(miss)
+        combo=0;
+        $("#Combo").html(combo)
+
+        if(miss>=GAMEOVER_MISS){
+            clearInterval(timer);
+            gameover=true;
+            $(".result .score").html(score);
+            $(".result .type").html(type);
+            $(".result .maxCombo").html(MaxCombo);
+            $(".result .miss").html(miss);
+            $(".result .missper").html((type==0?0:Math.floor(miss/type*10000)/100)+" %");
+            $(".result .result-rank").html(rank(score));
+
+            $(".result").show();
+        }
+    }
+    
+}
+    $("#nyuryoku").val("");
+})
+
+//お題を変える
+function themeset(){
+    rand=Math.floor(Math.random()*data.length);
+    $("#ja").html(data[rand].kana)
+    theme=data[rand].eigo;
+    createSpan();
+    strNum=0;
+    addUnder();
+}
+
+//下線をひく
+function addUnder(){
+    $("#theme span").removeClass("under");
+    $(`#theme span:nth-child(${strNum+1})`).addClass("under");
+}
+
+//得点・Missなどのメッセージを表示する
+function showmessage(message,color,top="80px",right="150px"){
+    $(".game-box").append(`<div class="scorePlus" style="color:${color};top:${top};right:${right}">${message}</div>`)
+    
+    $(".scorePlus").animate({top:"-100px",opacity:0},1000,"linear",()=>{
+        $(".scorePlus:first").remove();
+    });
+}
+
+function rank(scr){
+    switch(true){
+        case scr>24340000:{
+            return "2434ランク"
+        }
+        case scr>1000000:{
+            return "SSSランク";
+        }
+        case scr>800000:{
+            return "SSランク";
+        }
+        case scr>400000:{
+            return "Sランク";
+        }
+        case scr>100000:{
+            return "Aランク";
+        }
+        case scr>30000:{
+            return "Bランク";
+        }
+        case scr>10000:{
+            return "Cランク";
+        }
+        default :{
+            return "Dランク"
+        }
+    }
+}
+
+
+//フォーカスを固定する
+$("#nyuryoku").on("blur",()=>{
+    $("#nyuryoku").focus();
+})
+
+
+
+// let data2=[]
+
+// t=$("table tbody").children().length;
+
+// for(let i=0;i<t;i++){
+    //     kana=$(`table tbody tr:nth-child(${i+1}) th a`).text()
+    //     eigo=$(`table tbody tr:nth-child(${i+1}) td`).text()
+    
+    //     obj={
+        //         kana,
+        //         eigo
+        //     }
+//     data2.push(obj)
+// }
+
+// aaa=JSON.stringify(data2)
+// console.log(aaa)
